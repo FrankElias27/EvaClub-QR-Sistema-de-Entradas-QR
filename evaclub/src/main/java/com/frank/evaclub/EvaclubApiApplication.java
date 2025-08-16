f16b71d8ad757075cd814f6778335b79817e5dfa
FRANK <frankmascco@gmail.com>
2025-08-16 05:07:44 -0500

    first commit

---
+++ evaclub/src/main/java/com/frank/evaclub/EvaclubApiApplication.java
@@ -0,0 +1,13 @@
+package com.frank.evaclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaclubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaclubApiApplication.class, args);
+	}
+
+}
---
+++ evaclub/src/test/java/com/frank/evaclub/EvaclubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frank.evaclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaclubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
